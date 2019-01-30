fa8efc4322a31a9df742fceff69d2ff7400e9d5a
Alan Nunez <raul.nunez01@gmail.com>
2019-01-30 14:53:33 -0400

    Building GoalsVC

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  goalpost-app
 //
 //  Created by Desarrollo on 1/30/19.
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        print("Btn was presed")
+    }
 }
 
